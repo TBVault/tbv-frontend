ea05cf21e72c3524616c1f072bf8ed810536e969
Balaram Das Behera <balaramdb@gmail.com>
2025-12-15 15:21:04 -0800

    Update SVG icon in ChatSidebar for improved visual representation of chat functionality

--- src/components/ChatSidebar.tsx
+++ src/components/ChatSidebar.tsx
@@ -110,7 +110,7 @@ export default function ChatSidebar({ isOpen, onToggle }: ChatSidebarProps) {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth={2}
-                  d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+                  d="M12 4v16m8-8H4"
                 />
               </svg>
               New Chat
